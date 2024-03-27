c8bf1de9ce0fbeafed4be2149f0d90a9fc6a3ab9
Drex <nicknamedrex@gmail.com>
2024-03-27 20:21:36 +0100

    Update to 24w13a

--- src/main/java/me/drex/magic_particles/codec/CustomCodecs.java
+++ src/main/java/me/drex/magic_particles/codec/CustomCodecs.java
@@ -2,17 +2,12 @@ package me.drex.magic_particles.codec;
 
 import com.mojang.serialization.Codec;
 import net.minecraft.commands.arguments.EntityAnchorArgument;
-import net.minecraft.util.ExtraCodecs;
 
 public class CustomCodecs {
 
-    public static final Codec<EntityAnchorArgument.Anchor> ANCHOR = ExtraCodecs.stringResolverCodec(anchor -> {
-        return switch (anchor) {
-            case EYES -> "eyes";
-            case FEET -> "feet";
-            default -> throw new IncompatibleClassChangeError();
-        };
+    public static final Codec<EntityAnchorArgument.Anchor> ANCHOR = Codec.stringResolver(anchor -> switch (anchor) {
+        case EYES -> "eyes";
+        case FEET -> "feet";
     }, EntityAnchorArgument.Anchor::getByName);
-
-
+    
 }
