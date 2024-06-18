65d883f2f967d31b9f4e2aca51ac3b82e271ce8c
Drex <nicknamedrex@gmail.com>
2024-06-18 02:15:13 +0200

    Update to 1.21

--- src/main/java/me/drex/magic_particles/command/MagicParticlesCommand.java
+++ src/main/java/me/drex/magic_particles/command/MagicParticlesCommand.java
@@ -9,15 +9,16 @@ import com.mojang.brigadier.tree.LiteralCommandNode;
 import eu.pb4.playerdata.api.PlayerDataApi;
 import me.drex.magic_particles.particles.MagicParticle;
 import me.drex.magic_particles.particles.ParticleManager;
+import me.drex.message.api.LocalizedMessage;
+import me.drex.message.api.MessageAPI;
 import me.lucko.fabric.api.permissions.v0.Permissions;
-import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.SharedSuggestionProvider;
 import net.minecraft.nbt.StringTag;
-import net.minecraft.network.chat.*;
+import net.minecraft.network.chat.ComponentUtils;
+import net.minecraft.network.chat.MutableComponent;
 
-import java.util.Comparator;
 import java.util.Map;
 import java.util.function.Predicate;
 
@@ -30,71 +31,58 @@ public class MagicParticlesCommand {
 
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         LiteralCommandNode<CommandSourceStack> rootNode = dispatcher.register(
-                Commands.literal("magic-particles").requires(ROOT_PREDICATE)
-                        .executes(MagicParticlesCommand::sendList)
+            Commands.literal("magic-particles").requires(ROOT_PREDICATE)
+                .executes(MagicParticlesCommand::sendList)
+                .then(
+                    Commands.literal("set")
+                        .requires(Permissions.require("magic-particles.particle", 2))
                         .then(
-                                Commands.literal("set")
-                                        .requires(Permissions.require("magic-particles.particle", 2))
-                                        .then(
-                                                Commands.argument("particle", StringArgumentType.string()).suggests(SUGGEST_MAGIC_PARTICLES)
-                                                        .executes(MagicParticlesCommand::setParticle)
-                                        )
-                        ).then(
-                                Commands.literal("disable")
-                                        .requires(Permissions.require("magic-particles.disable", 2))
-                                        .executes(MagicParticlesCommand::disable)
-                        ).then(
-                                Commands.literal("reload")
-                                        .requires(Permissions.require("magic-particles.reload", 2))
-                                        .executes(MagicParticlesCommand::reload)
+                            Commands.argument("particle", StringArgumentType.string()).suggests(SUGGEST_MAGIC_PARTICLES)
+                                .executes(MagicParticlesCommand::setParticle)
                         )
+                ).then(
+                    Commands.literal("disable")
+                        .requires(Permissions.require("magic-particles.disable", 2))
+                        .executes(MagicParticlesCommand::disable)
+                ).then(
+                    Commands.literal("reload")
+                        .requires(Permissions.require("magic-particles.reload", 2))
+                        .executes(MagicParticlesCommand::reload)
+                )
         );
 
         dispatcher.register(Commands.literal("mp").executes(MagicParticlesCommand::sendList)
-                .requires(ROOT_PREDICATE).redirect(rootNode));
+            .requires(ROOT_PREDICATE).redirect(rootNode));
     }
 
     private static int sendList(CommandContext<CommandSourceStack> ctx) {
         Map<String, MagicParticle> particles = ParticleManager.particles();
-        MutableComponent component = Component.translatable("text.magic_particles.title",
-                        Component.literal(String.valueOf(particles.size())).withStyle(ChatFormatting.YELLOW)
-                ).withStyle(ChatFormatting.GOLD)
-                .append(
-                        Component.translatable("text.magic_particles.disable")
-                                .withStyle(ChatFormatting.RED)
-                                .withStyle(style -> style
-                                        .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/mp disable"))
-                                        .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.translatable("text.magic_particles.disable.hover").withStyle(ChatFormatting.RED)))
-                                )
-                ).append(Component.literal(" | ").withStyle(ChatFormatting.GRAY));
-        component.append(ComponentUtils.formatList(particles.entrySet().stream().sorted(Comparator.comparing(o -> o.getValue().name())).toList(), entry ->
-                Component.literal(entry.getValue().name())
-                        .withStyle(ChatFormatting.GREEN)
-                        .withStyle(style -> style.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                                                Component.literal(entry.getValue().name())
-                                                        .withStyle(ChatFormatting.GOLD)
-                                                        .append("\n")
-                                                        .append(Component.literal(entry.getKey()).withStyle(ChatFormatting.DARK_GRAY))
-                                                        .append("\n")
-                                                        .append(Component.translatable("text.magic_particles.particle.hover").withStyle(ChatFormatting.GREEN))
-                                        )
-                                ).withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/mp set " + entry.getKey()))
-                        ))
+
+        MutableComponent list = ComponentUtils.formatList(
+            particles.entrySet(),
+            LocalizedMessage.localized("text.magic_particles.overview.list.separator"),
+            particleEntry -> LocalizedMessage.builder("text.magic_particles.overview.list.element")
+                .addPlaceholder("id", particleEntry.getKey())
+                .addPlaceholder("name", particleEntry.getValue().name())
+                .build()
         );
-        ctx.getSource().sendSuccess(() -> component, false);
+
+        ctx.getSource().sendSuccess(() -> LocalizedMessage.builder("text.magic_particles.overview")
+            .addPlaceholder("count", particles.size())
+            .addPlaceholder("list", list)
+            .build(), false);
         return particles.size();
     }
 
 
     private static int reload(CommandContext<CommandSourceStack> ctx) {
+        MessageAPI.reload();
         if (ParticleManager.load()) {
             Map<String, MagicParticle> particles = ParticleManager.particles();
-            MutableComponent component = Component.translatable("text.magic_particles.reload").withStyle(ChatFormatting.GOLD);
-            ctx.getSource().sendSuccess(() -> component, false);
+            ctx.getSource().sendSuccess(() -> LocalizedMessage.localized("text.magic_particles.reload"), false);
             return particles.size();
         } else {
-            MutableComponent component = Component.translatable("text.magic_particles.error");
-            ctx.getSource().sendFailure(component);
+            ctx.getSource().sendFailure(LocalizedMessage.localized("text.magic_particles.error"));
         }
         return 1;
     }
@@ -102,20 +90,19 @@ public class MagicParticlesCommand {
     private static int setParticle(CommandContext<CommandSourceStack> ctx) throws CommandSyntaxException {
         String particle = StringArgumentType.getString(ctx, "particle");
         if (!ParticleManager.particles().containsKey(particle)) {
-            ctx.getSource().sendFailure(Component.translatable("text.magic_particles.unknown"));
+            ctx.getSource().sendFailure(LocalizedMessage.localized("text.magic_particles.unknown"));
             return 0;
         } else {
             PlayerDataApi.setGlobalDataFor(ctx.getSource().getPlayerOrException(), PARTICLE, StringTag.valueOf(particle));
-            MutableComponent component = Component.translatable("text.magic_particles.set", Component.literal(ParticleManager.particles().get(particle).name()).withStyle(ChatFormatting.YELLOW)).withStyle(ChatFormatting.GOLD);
-            ctx.getSource().sendSuccess(() -> component, false);
+            String name = ParticleManager.particles().get(particle).name();
+            ctx.getSource().sendSuccess(() -> LocalizedMessage.builder("text.magic_particles.set").addPlaceholder("id", particle).addPlaceholder("name", name).build(), false);
             return 1;
         }
     }
 
     private static int disable(CommandContext<CommandSourceStack> ctx) throws CommandSyntaxException {
         PlayerDataApi.setGlobalDataFor(ctx.getSource().getPlayerOrException(), PARTICLE, null);
-        MutableComponent component = Component.translatable("text.magic_particles.unset").withStyle(ChatFormatting.GOLD);
-        ctx.getSource().sendSuccess(() -> component, false);
+        ctx.getSource().sendSuccess(() -> LocalizedMessage.localized("text.magic_particles.unset"), false);
         return 1;
     }
 }
