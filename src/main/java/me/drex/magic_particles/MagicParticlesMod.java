ba0e2d9d6f8d7dbe890ee2af4e5772e8dee3f9f3
Drex <nicknamedrex@gmail.com>
2024-06-28 02:17:49 +0200

    Enable mod in singleplayer

--- src/main/java/me/drex/magic_particles/MagicParticlesMod.java
+++ src/main/java/me/drex/magic_particles/MagicParticlesMod.java
@@ -3,21 +3,21 @@ package me.drex.magic_particles;
 import me.drex.magic_particles.command.MagicParticlesCommand;
 import me.drex.magic_particles.particles.ParticleManager;
 import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MagicParticlesMod implements DedicatedServerModInitializer {
+public class MagicParticlesMod implements ModInitializer {
 
 	public static final String MOD_ID = "magic-particles";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static final ResourceLocation PARTICLE = ResourceLocation.fromNamespaceAndPath(MOD_ID, "particle");
 
 	@Override
-	public void onInitializeServer() {
+	public void onInitialize() {
 		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> MagicParticlesCommand.register(dispatcher));
 		ParticleManager.init();
 	}
-
 }
